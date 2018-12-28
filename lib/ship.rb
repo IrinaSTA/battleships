745337ff261fd6ba839d9361151d35911b32b712
Irina Baldwin <2007inb@gmail.com>
2018-12-28 10:23:59 +0000

    Add ship position

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,12 @@
 class Ship
 
-  attr_reader :size
+  attr_reader :size, :position
 
-  def initialize(size)
+  def initialize(size, position)
+    raise 'Ship cannot be bigger than 5' if size > 5
+    raise 'Ship cannot be smaller than 2' if size < 2
     @size = size
+    @position = position
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -4,10 +4,13 @@ require 'ship'
 # ship has to have coordinates
 describe Ship do
 
-  let(:ship) { Ship.new(5) }
+  let(:ship) { Ship.new(5, ['a1', 'a2', 'a3', 'a4', 'a5']) }
 
   it 'can have a size' do
     expect(ship.size).to eq(5)
   end
 
+  it 'can have a position on the board' do
+    expect(ship.position).to eq(['a1', 'a2', 'a3', 'a4', 'a5'])
+  end
 end
