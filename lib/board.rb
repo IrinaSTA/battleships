a13dd9984f6fe237e4b1a3d873033c38de4f7111
Irina Baldwin <2007inb@gmail.com>
2018-12-28 14:22:15 +0000

    Refactor Board variables for consistency and ease of reading

--- lib/board.rb
+++ lib/board.rb
@@ -3,32 +3,32 @@ class Board
   attr_reader :all_locations
 
   def initialize(x, y)
-    @width = x
-    @height = y
+    @x = x
+    @y = y
     # width/x is numerical, height/y is alphabetical
-    @all_locations = setup_board(@width, @height)
+    @all_locations = setup_board(@x, @y)
   end
 
   private
 
-  attr_reader :width, :height
+  attr_reader :x, :y
 
-  def setup_board(width, height)
+  def setup_board(x, y)
     all_locations = []
-    for each_letter in width_coords do
-      for each_num in height_coords do
+    for each_letter in y_coords do
+      for each_num in x_coords do
         all_locations << define_location(each_letter, each_num)
       end
     end
     all_locations
   end
 
-  def width_coords
-    ('a'..'z').to_a[0...width]
+  def y_coords
+    ('a'..'z').to_a[0...y]
   end
 
-  def height_coords
-    (1..height).to_a
+  def x_coords
+    (1..x).to_a
   end
 
   def define_location(y_coord, x_coord)
